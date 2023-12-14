d5d66d876127c2b2e92e6591e1d92e0c18ad6b8f
Sunf3r <sunf3r@proton.me>
2023-12-14 03:23:06 +0000

    add cmd helpful functions & comment them

--- classes/cmd.ts
+++ classes/cmd.ts
@@ -1,41 +1,50 @@
+import type { AnyMessageContent, proto } from 'baileys'
 import { getHeaders, request } from 'util/request.ts'
 import type { Msg } from 'types/types.d.ts'
-import { AnyMessageContent } from 'baileys'
 import { cmds, url } from 'ports'
 
 export default class Cmd {
 	name: str
 	aliases: str[]
 	cooldown: num // in secs
-	access: Partial<{
-		dm: bool // Can this cmd run on DMs?
-		groups: bool // Can this cmd run on groups?
-		onlyDevs: bool // Can only devs run this cmd?
-	}>
+	access: {
+		dm?: bool // Can this cmd run on DMs?
+		groups?: bool // Can this cmd run on groups?
+		onlyDevs?: bool // Can only devs run this cmd?
+	}
 	permissions: {
 		react?: num // How many times can this cmd react on msgs?
 		sendTxt?: num // How many txt msgs can this cmd send?
 		sendMedia?: num // How many media msgs can this cmd send?
 	}
-	params: cmdParams[]
+	params: cmdParams[] // Cmd will only receive params with these names from the main
 
-	run?(ctx: Headers): Promise<Response>
+	run?(ctx: Headers): Promise<Response> // Pretend this is an abstract function
 
 	constructor({ access, aliases, cooldown, permissions, params, name }: Partial<Cmd>) {
 		this.name = name as 'ping'
+		// 'as ping' bc i won't create a type with all cmd names
+		// and i'll need it later
+
 		this.aliases = aliases || []
 		this.cooldown = cooldown === 0 ? 0 : cooldown || 3
-		// Some cmds does not have cooldown
+		// if the cmd does not specifies it, it's 3
+		// but if it specifies to 0, the cmd won't have a cd delay
+
 		this.access = Object.assign({
-			dm: true,
-			groups: true,
-			onlyDevs: false,
-		}, access) // Compare cmd access
+			dm: true, // by default, all cmds can run on DMs
+			groups: true, // and on groups
+			onlyDevs: false, // and are accessible for everyone
+		}, access)
+		// It compares the cmd access options to the default options
+
 		this.permissions = Object.assign({
-			react: 0,
-			sendTxt: 0,
-			sendMedia: 0,
-		}, permissions) // Compare cmd permissions
+			react: 0, // by default, a cmd can't react on msgs
+			sendTxt: 0, // and can't send text msgs
+			sendMedia: 0, // and can't send media msgs
+		}, permissions)
+		// It compares the cmd permissions to the default permissions
+
 		this.params = params!
 
 		if (cmds[name as 'ping']) {
@@ -49,18 +58,51 @@ export default class Cmd {
 	public setup() {
 		const headers = getHeaders({
 			name: this.name,
+			access: this.access,
+			params: this.params,
 			aliases: this.aliases,
 			cooldown: this.cooldown,
-			access: this.access,
+			permissions: this.permissions,
 		})
 
 		return new Response(null, { headers, status: 200 })
 	}
+
 	// Send a message to a chat
 	public async send(id: str | Msg, body: str | AnyMessageContent) {
 		return await request(url + '/send', {
 			id,
 			body,
+		})
+	}
+
+	// React on a msg
+	public async react(msg: Msg, emoji: str) {
+		return await request(url + '/react', {
+			msg,
+			emoji,
+		})
+	}
+
+	// Edit a msg send by the bot
+	public async editMsg(msg: Msg, newText: str) {
+		return await request(url + '/edit', {
+			msg,
+			newText,
+		})
+	}
+
+	// Delete a msg *for everyone*
+	public async deleteMsg(msgOrKey: Msg | proto.IMessageKey) {
+		return await request(url + '/delete', {
+			msgOrKey,
+		})
+	}
+
+	// Download the media from a msg
+	public async downloadMedia(msg: Msg) {
+		return await request(url + '/downloadMedia', {
+			msg,
 		})
 	}
 }
--- util/request.ts
+++ util/request.ts
@@ -4,20 +4,20 @@ export { getHeaders, request }
 async function request(url: str, data: any) {
 	const headers = getHeaders(data)
 
-	const req = await fetch(url, { headers })
-
-	return req
+	return await fetch(url, { headers })
 }
 
 // getHeaders: generate & parse request headers
 function getHeaders(data: any) {
 	const strObj = {}
 
+	// stringify all objects inside 'data'
 	for (const [key, value] of Object.entries(data)) {
 		//@ts-ignore 'obj' does not have a type yet
 		strObj[key] = typeof value === 'object' ? JSON.stringify(value) : value
 	}
 
+	// create THE HEADERS OMG
 	return new Headers({
 		'Content-Type': 'application/json',
 		...strObj,
